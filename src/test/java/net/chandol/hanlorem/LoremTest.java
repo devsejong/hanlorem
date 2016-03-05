224c8b405cd2bb1cf25e569fe6d018955f738228
sejongpark <devsejong@gmail.com>
2016-03-05 22:32:04 +0900

    Initial commit

---
+++ src/test/java/net/chandol/hanlorem/LoremTest.java
@@ -0,0 +1,51 @@
+package net.chandol.hanlorem;
+
+import org.junit.Test;
+
+public class LoremTest {
+
+    @Test
+    public void testGetSentence() throws Exception {
+        String sentence = Lorem.getSentence();
+        System.out.println(sentence);
+
+        String sentences1 = Lorem.getSentences(4);
+        System.out.println(sentences1);
+
+        String sentences2 = Lorem.getSentences(2, 4);
+        System.out.println(sentences2);
+    }
+
+    @Test
+    public void testGetParagraph() throws Exception {
+        String paragraph = Lorem.getParagraph();
+        System.out.println(paragraph);
+
+        String paragraphs = Lorem.getParagraphs(4);
+        System.out.println(paragraphs);
+    }
+
+    @Test
+    public void testGetFirstName() throws Exception {
+        String firstName = Lorem.getFirstName();
+        System.out.println(firstName);
+    }
+
+    @Test
+    public void testGetLastName() throws Exception {
+        String lastName = Lorem.getLastName();
+        System.out.println(lastName);
+    }
+
+    @Test
+    public void testGetName() throws Exception {
+        String name = Lorem.getName();
+        System.out.println(name);
+    }
+
+    @Test
+    public void testGetPhoneNumber(){
+        String number = Lorem.getPhoneNumber();
+        System.out.println(number);
+    }
+}
